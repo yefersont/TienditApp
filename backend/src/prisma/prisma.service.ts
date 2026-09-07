cfe43d879b55d26e6734822775c230cf38c8725f
Yeferson <yefertello2002@gmail.com>
2026-09-07 01:07:33 -0500

    Corregir import de Prisma Client

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '../../generated/prisma/client';
+
 @Injectable()
 export class PrismaService
     extends PrismaClient
